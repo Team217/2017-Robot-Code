026ebf466d50275be39d8ab5c9fafadd4c9fd9a7
TylerCarrier <Carriermtyler@gmail.com>
2017-02-01 18:44:05 -0500

    Add files via upload
    Expanded upon Hayden Skeleton, includes values for butterfree drivebase.

--- Robot.java
+++ Robot.java
@@ -1,187 +1,307 @@
+
 package org.usfirst.frc.team217.robot;
 
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.interfaces.Gyro;
-import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.CANSpeedController.ControlMode;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.ni.vision.NIVision;
+import com.ni.vision.NIVision.Image;
 import com.ctre.*;
 import com.ctre.CANTalon.FeedbackDevice;
 import com.ctre.CANTalon.TalonControlMode;
 
-import edu.wpi.first.wpilibj.*;
 
 /**
- * The VM is configured to automatically run this class, and to call the
+\* The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
  * documentation. If you change the name of this class or the package after
  * creating this project, you must also update the manifest file in the resource
  * directory.
  */
 public class Robot extends IterativeRobot {
-	final String defaultAuto = "Default";
-	final String customAuto = "My Auto";
-	String autoSelected;
-	CANTalon FR, FL, BR, BL;
 	
-	ADXRS450_Gyro Hgyro;
-	Joystick Driver, Op;
+	//Joysticks
+	
+	Joystick driver, oper;
+	
+	//Drive talons
+	
+	CANTalon frontleft, frontright, backleft, backright;
+	
+	//shooting talons
+	
+	CANTalon turretRotation, flywheel, hood, ballLift, intake, funnel;
+	
+	//climbing talons
+	
+	CANTalon climber, climber2;
+	
+	//Solenoids
+	
+	DoubleSolenoid frontrightsolenoid, frontleftsolenoid, backleftsolenoid, backrightsolenoid;
+	
+	//Timers
+	
+	//Gyro
+	
+	AnalogGyro horzGyro;
+	
+	//shooter PID
+	
+	double shooterWheelSpeed;
+	double targetspeed;
+	
+	boolean isatspeed = false;
+	
+	//PID
+	
+	double PIDError = 0;
+	double cumPIDError = 0;
+	
+	//auton variables
 	boolean SafeCheck, ballsOut, gearGone;
-	
-	enum AutonEnum{
-		Fire10,
-		placeGear,
-		LoadBalls,
-		FireMore,
-		stop
-	};
-	
-	AutonEnum AutonState = AutonEnum.Fire10;
-	
-	enum StartPosEnum{
-		Spos1,
-		Spos2,
-		Spos3
-	};
-	
-	StartPosEnum SPos;
-	
-	enum GearPosEnum{
-		Gpos1,
-		Gpos2,
-		Gpos3
-	};
-	
-	GearPosEnum GPos;
-	
-	enum SideEnum{
-		red,
-		blue
-	};
-	
-	SideEnum Side;
-	
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
-	@Override
-	public void robotInit() {
-		FR = new CANTalon(0);
-		FL = new CANTalon(1);
-		BR = new CANTalon(2);
-		BL = new CANTalon(3);
-		
-		Driver = new Joystick(0);
-		Op = new Joystick(1);
-		
-		Hgyro = new ADXRS450_Gyro();
-		
-	}
+	 	
+	 	enum AutonEnum{
+	 		Fire10,
+	 		placeGear,
+	 		LoadBalls,
+	 		FireMore,
+	 		stop
+	 	};
+	 	
+	 	AutonEnum AutonState = AutonEnum.Fire10;
+	 	
+	 	enum StartPosEnum{
+	 		Spos1,
+	 		Spos2,
+	 		Spos3
+	 	};
+	 	
+	 	StartPosEnum SPos;
+	 	
+	 	enum GearPosEnum{
+	 		Gpos1,
+	 		Gpos2,
+	 		Gpos3
+	 	};
+	 	
+	 	GearPosEnum GPos;
+	 	
+	 	enum SideEnum{
+	 		red,
+	 		blue
+	 	};
+	 	
+	 	SideEnum Side;
+	 	
+	 	enum shootingenum{
+	 		
+	 	}
+	 	
 
+	
+    public void robotInit() {
+    	
+    	// Joysticks
+    	driver = new Joystick(0);
+    	oper = new Joystick(1);
+    	
+    	// Drive Motor Controllers
+    	frontleft = new CANTalon(9);
+    	backleft = new CANTalon(6);
+    	backleft.changeControlMode(TalonControlMode.Follower);
+    	backleft.set(9);
+    	
+    	frontright = new CANTalon(2);
+    	backright = new CANTalon(0);
+    	backright.changeControlMode(TalonControlMode.Follower);
+    	backright.set(2);
+    	
+    	frontleft.setFeedbackDevice(FeedbackDevice.QuadEncoder);
+    	frontright.setFeedbackDevice(FeedbackDevice.QuadEncoder);
+   
+    	//Solenoids
+    	frontleftsolenoid = new DoubleSolenoid(0, 1);
+    	frontrightsolenoid = new DoubleSolenoid(6, 7);
+    	backleftsolenoid = new DoubleSolenoid(2, 3);
+    	backrightsolenoid = new DoubleSolenoid(4, 5);
+    	    	
+    	//Gyro
+    	horzGyro = new AnalogGyro(0);
+    	
+    	//Shooting Talons
+    	turretRotation = new CANTalon(10);
+    	flywheel = new CANTalon(11);
+    	hood = new CANTalon(12);
+    	ballLift = new CANTalon(13);
+    	intake = new CANTalon(14);
+    	funnel = new CANTalon(15);
+    	
+    	//Climber Talons
+    	climber = new CANTalon(16);
+    	climber2 = new CANTalon(17);
+        
+    }
+    
 	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString line to get the auto name from the text box below the Gyro
+	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
+	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
+	 * Dashboard, remove all of the chooser code and uncomment the getString line to get the auto name from the text box
+	 * below the Gyro
 	 *
-	 * You can add additional auto modes by adding additional comparisons to the
-	 * switch structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as well.
+	 * You can add additional auto modes by adding additional comparisons to the switch structure below with additional strings.
+	 * If using the SendableChooser make sure to add them to the chooser code above as well.
 	 */
-	@Override
-	public void autonomousInit() {
-		
-		Hgyro.reset();
-		
-	}
+    public void autonomousInit() {
+    	horzGyro.reset();
+    	frontright.setEncPosition(0);
+    	frontleft.setEncPosition(0);
+    }
 
-	/**
-	 * This function is called periodically during autonomous
-	 */
-	@Override
-	public void autonomousPeriodic() {
-		switch (autoSelected) {
-		case customAuto:
-			// Put custom auto code here
-			break;
-		case defaultAuto:
-		default:
-			// Put default auto code here
+    /**
+     * This function is called periodically during autonomous
+     */
+    public void autonomousPeriodic() {
+    
+    	switch(AutonState){
+		case Fire10:
 			
-			switch(AutonState){
-				case Fire10:
-					
-					Fire10();
-					
-					if(ballsOut){ //Check if we are doing ballsOut auton
-										//ballsOut is the name for 40 point auton
-						
-						AutonState = AutonEnum.LoadBalls;
-						
-					}
-					else{
-						
-						AutonState = AutonEnum.placeGear;
-						
-					}
-					break;
-				case placeGear:
-					
-					PlaceGear();
-					
-					if(gearGone){ //Check if the gear has been raised from the robot
-						
-						AutonState = AutonEnum.stop;
-						
-					}
-					
-					break;
-				case LoadBalls:
-					
-					LoadBalls();
-					
-					break;
-				case FireMore:
-					
-					FireMore();
-					
-					AutonState = AutonEnum.stop;
-					
-					break;
-					
-				case stop:
-					
-					FR.set(0);
-					FL.set(0);
-					BR.set(0);
-					BL.set(0);
-					
-					break;
-					
+			Fire10();
+			
+			if(ballsOut){ //Check if we are doing ballsOut auton
+								//ballsOut is the name for 40 point auton
+				
+				AutonState = AutonEnum.LoadBalls;
+				
+			}
+			else{
+				
+				AutonState = AutonEnum.placeGear;
+				
+			}
+			break;
+		case placeGear:
+			
+			PlaceGear();
+			
+			if(gearGone){ //Check if the gear has been raised from the robot
+				
+				AutonState = AutonEnum.stop;
+				
 			}
 			
+			break;
+		case LoadBalls:
 			
+			LoadBalls();
 			
 			break;
+		case FireMore:
+			
+			FireMore();
+			
+			AutonState = AutonEnum.stop;
+			
+			break;
+			
+		case stop:
+			
+			frontright.set(0);
+			frontleft.set(0);
+			backright.set(0);
+			backleft.set(0);
+			
+			break;
+			
+	}
+          
+       System.out.println("Left:" + frontleft.getEncPosition() + "   " + "Right:" + frontright.getEncPosition());
+          
+    }        
+  
+    public void teleopInit(){
+    	//This may need to be changed depending on whether omni is reverse or forward
+    	
+    	frontleftsolenoid.set(DoubleSolenoid.Value.kReverse);
+		frontrightsolenoid.set(DoubleSolenoid.Value.kReverse);
+		backleftsolenoid.set(DoubleSolenoid.Value.kReverse);
+		backrightsolenoid.set(DoubleSolenoid.Value.kReverse);
+		
+		frontright.setEncPosition(0);
+    	frontleft.setEncPosition(0);
+		
+		
+
+    	
+    }
+
+    
+    public void teleopPeriodic() {
+    	
+    	if(driver.getRawButton(5)){
+    		frontleftsolenoid.set(DoubleSolenoid.Value.kForward);
+    		frontrightsolenoid.set(DoubleSolenoid.Value.kForward);
+    		backleftsolenoid.set(DoubleSolenoid.Value.kForward);
+    		backrightsolenoid.set(DoubleSolenoid.Value.kForward);
+    	}
+    	
+    	if(driver.getRawButton(6)){
+    		frontleftsolenoid.set(DoubleSolenoid.Value.kReverse);
+    		frontrightsolenoid.set(DoubleSolenoid.Value.kReverse);
+    		backleftsolenoid.set(DoubleSolenoid.Value.kReverse);
+    		backrightsolenoid.set(DoubleSolenoid.Value.kReverse);
+    	}
+    	
+    	
+    	
+    	drivebase();
+    	
+        System.out.println("Left:" + frontleft.getEncPosition() + "   " + "Right:" + frontright.getEncPosition());
+
+
+    	if(driver.getRawButton(14)){
+    		frontright.setEncPosition(0);
+    		frontleft.setEncPosition(0);
+    	}
+    	
+    	
+    }
+
+    public void testPeriodic() {
+    	
+    	
+    	
+    	
+    
+    }
+    
+    //When PIDing drivebase movement
+    //left use:  -normPID, all the rest positive
+    //right use: -normPID, -myTar
+    double normPID(double myTar, double myPos, double myP, double myI) {
+    	PIDError = myTar - myPos;
+		cumPIDError = PIDError;
+		double PIDPout = PIDError * myP;
+		double PIDIout = PIDError * myI;
+		double PIDSpeed = (PIDPout + PIDIout);
+
+		if (absVal(PIDError) < 5) {
+			PIDSpeed = 0;
 		}
-	}
-
-	/**
-	 * This function is called periodically during operator control
-	 */
-	@Override
-	public void teleopPeriodic() {
-	}
-
-	/**
-	 * This function is called periodically during test mode
-	 */
-	@Override
-	public void testPeriodic() {
-	}
-	
-	public void Fire10() {
+		return (PIDSpeed);
+    }
+    
+    void drivebase(){
+    	double speed = deadband(-driver.getY());
+    	double turn = deadband(driver.getZ());
+    		
+    		frontleft.set(-speed + -turn);
+    		frontright.set(speed + -turn);
+    	}
+    
+    
+   void Fire10() {
 		//Auton method for firing the first 10
 		switch(Side)
 		{
@@ -215,7 +335,7 @@ public class Robot extends IterativeRobot {
 		}
 	}
 	
-	public void PlaceGear() {
+	void PlaceGear() {
 		//Auton method for driving up and placing gear
 		
 		switch(GPos)
@@ -230,7 +350,7 @@ public class Robot extends IterativeRobot {
 		
 	}
 	
-	public void LoadBalls() {
+	void LoadBalls() {
 		//Auton method to drive to the hopper and load balls
 		
 		switch(Side)
@@ -266,12 +386,50 @@ public class Robot extends IterativeRobot {
 		
 	}
 	
-	public void FireMore() {
+	void FireMore() {
 		//Auton method to fire the balls from the hopper
+
 		
+	}
+	
+	void ballvision(){
+		//Does alignment to the boiler for shooting
+		//Align turret
+		//Align hood
+	}
+	
+	void gearvision(){
+		//Does alignment to the gear for docking
+		//Moves the bot until aligned to vision coordinates
+		//Controls driving to dock perfectly
+	}
+	
+	void shooting(){
+		//This method contains all of the shooting code
+		
+		//align the turret according to vision values
+		//align the hood according to vision values
+		//run flywheel motor 
 		
 		
 	}
 	
-}
 
+    
+    
+    double deadband(double input) {
+
+		if (absVal(input) < .08) {
+			return 0;
+		}else
+			return input;	}
+
+		
+    double absVal(double input){
+	if (input < 0){
+		return -input;
+	}else
+		return input;
+}
+    
+}
